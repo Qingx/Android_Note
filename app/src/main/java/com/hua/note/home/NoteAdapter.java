d1b21a672c7f203a8c5322fe5002dcdffea0cb2c
Iaovy <Cymbidium@outlook.com>
2020-03-31 18:13:04 +0800

    default

--- app/src/main/java/com/hua/note/home/NoteAdapter.java
+++ app/src/main/java/com/hua/note/home/NoteAdapter.java
@@ -10,6 +10,7 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.FragmentActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -25,9 +26,11 @@ import com.hua.note.data.UserDaoManager;
 
 import org.greenrobot.eventbus.EventBus;
 
+import java.sql.Time;
 import java.util.List;
 
 import cn.wl.android.lib.ui.BaseActivity;
+import cn.wl.android.lib.utils.Times;
 
 /**
  * Created by Iaovy on 2020/3/27 14:37
@@ -69,25 +72,41 @@ public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.ItemViewHolder
                 }
         );
         holder.itemView.setOnLongClickListener(v -> {
-            new AlertDialog.Builder(v.getContext())
-                    .setMessage("删除这条便签")
-                    .setNegativeButton("取消", (dialog, which) -> {
-                        /**
-                         * 置顶方法
-                         */
-                    })
-                    .setPositiveButton("置顶", (dialog, which) -> {
-                        NoteEntity noteEntity = entities.get(position);
-                        StickyEntity stickyEntity = new StickyEntity(noteEntity.getId(), noteEntity.getTime(), noteEntity.getText()+"sticky", noteEntity.getType(), "sticky");
-                        userDaoManager.stickyNote(stickyEntity);
+            NoteEntity noteEntity = entities.get(position);
 
-                        userDaoManager.deleteNote(noteEntity);
-                        entities.remove(noteEntity);
-                        notifyItemRemoved(position);
-                        notifyDataSetChanged();
-                        EventBus.getDefault().post(new MessageEvent("updateAdapter"));
-                    })
-                    .create().show();
+            AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
+            View view = View.inflate(v.getContext(), R.layout.dialog_note, null);
+            builder.setView(view);
+            builder.setCancelable(true);
+            AlertDialog dialog = builder.create();
+            dialog.show();
+
+            /**
+             * 置顶按钮
+             */
+            ConstraintLayout stickyLayout = view.findViewById(R.id.layout_sticky);
+            stickyLayout.setOnClickListener(v1 -> {
+                StickyEntity stickyEntity = new StickyEntity(noteEntity.getId(), Times.current(), noteEntity.getText(), noteEntity.getType(), "sticky");
+                userDaoManager.stickyNote(stickyEntity);
+                userDaoManager.deleteNote(noteEntity);
+                entities.remove(noteEntity);
+                notifyItemRemoved(position);
+                notifyDataSetChanged();
+                EventBus.getDefault().post(new MessageEvent("updateAdapter"));
+                dialog.dismiss();
+            });
+            /**
+             * 删除按钮
+             */
+            ConstraintLayout removeLayout = view.findViewById(R.id.layout_remove);
+            removeLayout.setOnClickListener(v1 -> {
+                userDaoManager.deleteNote(noteEntity);
+                entities.remove(noteEntity);
+                notifyItemRemoved(position);
+                notifyDataSetChanged();
+                dialog.dismiss();
+            });
+
             return false;
         });
     }
--- app/src/main/java/com/hua/note/home/StickyAdapter.java
+++ app/src/main/java/com/hua/note/home/StickyAdapter.java
@@ -9,19 +9,25 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.chad.library.adapter.base.BaseViewHolder;
 import com.hua.note.R;
 import com.hua.note.config.DateFormat;
+import com.hua.note.config.MessageEvent;
 import com.hua.note.config.Tools;
 import com.hua.note.create.CreateActivity;
+import com.hua.note.data.NoteEntity;
 import com.hua.note.data.StickyEntity;
 import com.hua.note.data.UserDaoManager;
+
+import org.greenrobot.eventbus.EventBus;
 
 import java.util.List;
 
 import cn.wl.android.lib.ui.BaseActivity;
+import cn.wl.android.lib.utils.Times;
 
 /**
  * Created by Iaovy on 2020/3/27 14:37
@@ -58,22 +64,48 @@ public class StickyAdapter extends RecyclerView.Adapter<StickyAdapter.ItemViewHo
         holder.text.setText(Tools.cutStr(entities.get(position).getText(), 16));
         holder.time.setText(DateFormat.yearMonthDayTime(entities.get(position).getTime()) + " " + Tools.getWeekDays());
         holder.itemView.setOnClickListener(v -> {
-                    CreateActivity.Companion.start(context, entities.get(position).getId(),"sticky");
+                    CreateActivity.Companion.start(context, entities.get(position).getId(), "sticky");
                     ((BaseActivity) v.getContext()).overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
                 }
         );
         holder.itemView.setOnLongClickListener(v -> {
-            new AlertDialog.Builder(v.getContext())
-                    .setMessage("删除这条便签")
-                    .setNegativeButton("取消", (dialog, which) -> {
-                    })
-                    .setPositiveButton("删除", (dialog, which) -> {
-                        userDaoManager.deletesticky(entities.get(position));
-                        entities.remove(entities.get(position));
-                        notifyItemRemoved(position);
-                        notifyDataSetChanged();
-                    })
-                    .create().show();
+            StickyEntity stickyEntity = entities.get(position);
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
+            View view = View.inflate(v.getContext(), R.layout.dialog_note, null);
+            builder.setView(view);
+            builder.setCancelable(true);
+            AlertDialog dialog = builder.create();
+            dialog.show();
+            /**
+             * 取消置顶
+             */
+            ConstraintLayout stickyLayout = view.findViewById(R.id.layout_sticky);
+            TextView cancelSticky = view.findViewById(R.id.text_sticky);
+            cancelSticky.setText("取消置顶");
+            stickyLayout.setOnClickListener(v1 -> {
+                NoteEntity noteEntity = new NoteEntity(stickyEntity.getId(), Times.current(), stickyEntity.getText(), stickyEntity.getType(), "default");
+                userDaoManager.insertNote(noteEntity);
+                userDaoManager.deletesticky(stickyEntity);
+                entities.remove(stickyEntity);
+                notifyItemRemoved(position);
+                notifyDataSetChanged();
+                EventBus.getDefault().post(new MessageEvent("updateAdapter"));
+                dialog.dismiss();
+            });
+            /**
+             * 删除按钮
+             */
+            ConstraintLayout removeLayout = view.findViewById(R.id.layout_remove);
+            removeLayout.setOnClickListener(v1 -> {
+                userDaoManager.deletesticky(stickyEntity);
+                entities.remove(stickyEntity);
+                notifyItemRemoved(position);
+                notifyDataSetChanged();
+                dialog.dismiss();
+            });
+
+
             return false;
         });
     }
